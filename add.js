a0814924277d85fc49ddbfd21f981be47a4de93d
Kacper Radzik <KacperRadzik@op.pl>
2023-08-21 21:52:50 +0200

    my first repository

---
+++ add.js
@@ -0,0 +1,24 @@
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 50,
+    gutter: 10
+  });
+  $(".searchbtn").click(function(){
+    $(this).toggleClass("bg-green");
+    $(".fas").toggleClass("color-white");
+    $(".input").focus().toggleClass("active-width").val('');
+  });
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
